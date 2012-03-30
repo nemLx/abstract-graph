14bcc6478fdb706f0c09aaa1dc3748362b04e925
Dennis.McWherter@gmail.com <Dennis.McWherter@gmail.com@0e9e054e-b845-e123-f86b-6d814dca1af7>
2012-03-30 20:41:59 +0000

    Updated menu items. Added OpenGL window/tab stub

---
+++ Gui/Windows/GLWindow.cpp
@@ -0,0 +1,52 @@
+/**
+ * Application: AbstractGraph
+ * Module: GUI
+ * File: GLWindow.cpp
+ * Description: Implementation for OpenGL window
+ *
+ * @author Dennis J. McWherter, Jr.
+ * @version $Id$
+ */
+
+#include "GLWindow.h"
+
+GLWindow::GLWindow(QWidget* parent)
+  : QGLWidget(parent)
+{
+}
+
+GLWindow::~GLWindow()
+{
+  // Not implemented
+}
+
+void GLWindow::initalizeGL()
+{
+  // Not implemented
+}
+
+void GLWindow::resizeGL(int w, int h)
+{
+  // Not implemented
+}
+
+void GLWindow::paintGL()
+{
+  // Not implemented
+}
+
+void GLWindow::mousePressEvent(QMouseEvent* evt)
+{
+  // Not implemented
+}
+
+void GLWindow::mouseMoveEvent(QMouseEvent* evt)
+{
+  // Not implemented
+}
+
+void GLWindow::keyPressEvent(QKeyEvent* evt)
+{
+  // Not implemented
+}
+
