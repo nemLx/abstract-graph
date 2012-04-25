53d5b9e42ac80bc46acdd521630e826d659f1b86
ljgnemo@gmail.com <ljgnemo@gmail.com@0e9e054e-b845-e123-f86b-6d814dca1af7>
2012-04-25 04:11:15 +0000

    added more usage comments

--- trunk/AbstractGraph/include/Graphs/Graph.h
+++ trunk/AbstractGraph/include/Graphs/Graph.h
@@ -62,6 +62,10 @@ public:
 	 *
 	 * return:	length of odd cycle when there is one
 	 *			0 when there is no odd cycle
+	 * 
+	 * GUI:	highlight all edges with ids listed in cycle
+	 *		if there is an odd cycle, notify user when
+	 *		there is no cycle
 	 */
 	int getOddCycle(vector<int> * cycle);
 	
@@ -74,6 +78,10 @@ public:
 	 *
 	 * return:	true if there is eulerian path between s and t
 	 *			false if there is not
+	 *
+	 * GUI:	label edges with their order of appearance as
+	 *		ordered in the vector path, when there is Eulerian
+	 *		path between s,t. Notify user when there is not.
 	 */
 	bool eulerianPath(int s, int t, vector<int> * path);
 	
@@ -85,6 +93,9 @@ public:
 	 *
 	 * return:	number of bridges in the graph
 	 *			0 if there isn't any
+	 *
+	 * GUI:	highlight edges in bridges, if there any bridge
+	 *		is found. Notify user when no bridge is found.
 	 */
 	int getBridges(vector<int> * bridges);
 	
@@ -101,6 +112,10 @@ public:
 	 *			is properly encoded
 	 *			0 id the graph is not a tree and no prufer
 	 *			code exists
+	 * 
+	 * GUI:	display the code and label to the user, either
+	 *		on canvas or through a popup, if the graph is a
+	 *		tree. Notify user if it is not a tree.
 	 */
 	int getPruferCode(vector<int> * code, map<int, int> * label);
 	
@@ -109,6 +124,12 @@ public:
 	/*
 	 * code:	pointer to vector of ints used to store
 	 *			prufer code of the tree
+	 *
+	 * GUI:	this method should be treated as a way of graph
+	 *		construction. The resulting graph should be able
+	 *		to be manipulated by the user just as the ones
+	 *		created by hand or imported through xml. Also it
+	 *		treats the graph as empty.
 	 */
 	void buildFromPruferCode(vector<int> * code);
 	
@@ -120,7 +141,19 @@ public:
 	 *
 	 * dist:	the distance between two nodes
 	 *
-	 * 
+	 * GUI:	After this method is executed, you should retrieve
+	 *		shortest path information from path and dist, instead
+	 *		of running the individual pair shortest path each time.
+	 *
+	 * path usage:	if the shortest path between 1 and 8 is
+	 *				1->5->2->9->8 then:
+	 *				path[(1,8)] = 5
+	 *				path[(5,8)] = 2
+	 *				path[(2,8)] = 9
+	 *				path[(9,8)] = 8
+	 *
+	 *				also notice the shortest path between 5 and 8
+	 *				is exactly 5->2->9->8
 	 */
 	void allPairSP(map<pair<int, int>, int> * path, map<pair<int, int>, int> *dist);
 	
@@ -132,6 +165,12 @@ public:
 	 *
 	 * return:	1 if the sequence is graphic, and populates the graph
 	 *			0 if the sequence is not graphic, does nothing
+	 *
+	 * GUI:	this method should be treated as a way of graph
+	 *		construction. The resulting graph should be able
+	 *		to be manipulated by the user just as the ones
+	 *		created by hand or imported through xml. Also it
+	 *		treats the graph as empty.
 	 */
 	int buildFromSequence(vector<int> * sequence);
 	
--- trunk/AbstractGraph/src/Algorithms/GraphicSequence.cpp
+++ trunk/AbstractGraph/src/Algorithms/GraphicSequence.cpp
@@ -22,20 +22,29 @@ GraphicSequence::GraphicSequence(Graph * g, vector<int> * seq){
 
 
 
+/*
+ * Recursively checking graphic sequence satisfiability
+ * by removing the maximum degree d and subtracting 1 from
+ * next d largest numbers
+ */
 bool GraphicSequence::isGraphic(vector<int> * s){
 	
+	// base case, one node, degree 0
 	if ((int)s->size() == 1 && *s->begin() == 0){
 		return true;
 	}
 	
+	// sort, retrieve max and remove max
 	sort(s->begin(), s->end(), vectorReverse);
 	int max = *s->begin();
 	s->erase(s->begin());
 	
+	// no enough nodes left, not graphic
 	if ((int)s->size() < max){
 		return false;
 	}
 	
+	// enough nodes left, reduce degree and recurse
 	for (int i = 0; i < max; i++) {
 		s->at(i)--;
 	}
@@ -45,8 +54,13 @@ bool GraphicSequence::isGraphic(vector<int> * s){
 
 
 
+/*
+ * constructs the simple graph when seq is determined
+ * to be graphic
+ */
 void GraphicSequence::constructGraph(){
 	
+	// maps the initial indices to each node
 	vector< pair<int, int> > degreeList;
 	
 	for (int i = 0; i < n; i++) {
@@ -68,8 +82,13 @@ void GraphicSequence::addNodes(){
 
 
 
+/*
+ * recursively add edges between the node with
+ * highest degree d and the next d largest nodes
+ */
 void GraphicSequence::addEdges(vector< pair<int, int> > *s){
 
+	// single isolated nodes, base case
 	if ((int)s->size() == 1 && s->begin()->second == 0){
 		return;
 	}
@@ -79,6 +98,7 @@ void GraphicSequence::addEdges(vector< pair<int, int> > *s){
 	int curr = s->begin()->first;
 	s->erase(s->begin());
 	
+	// add max edges with the next max nodes 
 	for (int i = 0; i < max; i++) {
 		s->at(i).second--;
 		g->addEdge(curr, s->at(i).first);
