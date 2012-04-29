3772770efb8f196913729412a09e3e0ae4330aff
ljgnemo@gmail.com <ljgnemo@gmail.com@0e9e054e-b845-e123-f86b-6d814dca1af7>
2012-04-29 16:22:21 +0000

    fixed bug in graphic sequence

--- trunk/AbstractGraph/graphTest.cpp
+++ trunk/AbstractGraph/graphTest.cpp
@@ -421,30 +421,42 @@ void pruferDecodeTest(){
 
 void scTest(){
 	
-	DiGraph g(13);
+//	DiGraph g(13);
+//	
+//	g.addEdge(0,2);
+//	g.addEdge(1,0);
+//	g.addEdge(2,3);
+//	g.addEdge(2,4);
+//	g.addEdge(3,2);
+//	g.addEdge(3,4);
+//	g.addEdge(4,5);
+//	g.addEdge(4,6);
+//	g.addEdge(5,0);
+//	g.addEdge(5,3);
+//	g.addEdge(6,0);
+//	g.addEdge(6,7);
+//	g.addEdge(7,8);
+//	g.addEdge(8,7);
+//	g.addEdge(9,6);
+//	g.addEdge(9,8);
+//	g.addEdge(9,12);
+//	g.addEdge(10,9);
+//	g.addEdge(11,4);
+//	g.addEdge(11,9);
+//	g.addEdge(12,10);
+//	g.addEdge(12,11);
 	
-	g.addEdge(0,2);
-	g.addEdge(1,0);
-	g.addEdge(2,3);
-	g.addEdge(2,4);
-	g.addEdge(3,2);
-	g.addEdge(3,4);
-	g.addEdge(4,5);
-	g.addEdge(4,6);
-	g.addEdge(5,0);
-	g.addEdge(5,3);
-	g.addEdge(6,0);
-	g.addEdge(6,7);
-	g.addEdge(7,8);
-	g.addEdge(8,7);
-	g.addEdge(9,6);
-	g.addEdge(9,8);
-	g.addEdge(9,12);
-	g.addEdge(10,9);
-	g.addEdge(11,4);
-	g.addEdge(11,9);
-	g.addEdge(12,10);
-	g.addEdge(12,11);
+	DiGraph g(8);
+	
+	g.addEdge(0, 1);
+	g.addEdge(1, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 0);
+	g.addEdge(2, 4);
+	g.addEdge(4, 5);
+	g.addEdge(5, 6);
+	g.addEdge(6, 4);
+	g.addEdge(6, 7);
 	
 	printf("\n");
 	g.printGraph();
@@ -509,13 +521,17 @@ void graphicSequenceTest(){
 	
 	vector<int> s;
 	
-	s.push_back(3);
-	s.push_back(3);
-	s.push_back(3);
-	s.push_back(3);
-	s.push_back(3);
-	s.push_back(2);
-	s.push_back(2);
+//	s.push_back(3);
+//	s.push_back(3);
+//	s.push_back(3);
+//	s.push_back(3);
+//	s.push_back(3);
+//	s.push_back(2);
+//	s.push_back(2);
+	s.push_back(1);
+	s.push_back(1);
+	s.push_back(1);
+	s.push_back(1);
 	s.push_back(1);
 	
 	int isGraphic = g.buildFromSequence(&s);
@@ -532,30 +548,30 @@ void graphicSequenceTest(){
 
 int main(){
 	
-	shortestPathTest();
-	
-	mstTest();
-	
-	bipartiteTest();
-	
-	maxFlowTest();
-	
-	bipartiteMatchingTest();
-	
-	oddCycleTest();
-
-	eulerianPathTest();
-	
-	bridgeTest();
-	
-	pruferEncodeTest();
-
-	pruferDecodeTest();
-	
-	scTest();
-
-	allPairSPTest();
-
+//	shortestPathTest();
+//	
+//	mstTest();
+//	
+//	bipartiteTest();
+//	
+//	maxFlowTest();
+//	
+//	bipartiteMatchingTest();
+//	
+//	oddCycleTest();
+//
+//	eulerianPathTest();
+//	
+//	bridgeTest();
+//	
+//	pruferEncodeTest();
+//
+//	pruferDecodeTest();
+//	
+//	scTest();
+//
+//	allPairSPTest();
+//
 	graphicSequenceTest();
 	
 	return 0;
